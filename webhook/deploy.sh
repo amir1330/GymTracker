5d797618e6d254f9841cf644b546e644cb8fed7e
amir <iskakovamir7486@gmail.com>
2026-07-27 17:57:17 +0500

    Fix: nginx proxy to port 8080, deploy script uses docker-compose v1, remove webhook from compose

--- webhook/deploy.sh
+++ webhook/deploy.sh
@@ -1,5 +1,5 @@
-#!/bin/bash
+#!/bin/sh
 cd /root/gym-tracker
-docker compose -f docker-compose.prod.yml pull
-docker compose -f docker-compose.prod.yml up -d
+docker-compose -f docker-compose.prod.yml pull gym-tracker db
+docker-compose -f docker-compose.prod.yml up -d gym-tracker db
 docker image prune -f
